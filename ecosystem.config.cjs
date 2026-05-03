2d44cbd7d4c3e2e7b0b57af81e4c5ad34b0bd926
Anton Seredin <seredin.anton2014@gmail.com>
2026-05-04 02:47:24 +0300

    greate updates

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,13 +3,16 @@ require("dotenv").config()
 module.exports = {
   apps: [
     {
-      name: "nuxt-app",
+      name: "novoe-zhegalovo",
       script: ".output/server/index.mjs",
+
       env: {
         NODE_ENV: "production",
         PORT: 3000,
+
         YANDEX_MAPS_API_KEY: process.env.YANDEX_MAPS_API_KEY,
         NUXT_APP_BASE_URL: process.env.NUXT_APP_BASE_URL,
+
         YANDEX_MAIL_USER: process.env.YANDEX_MAIL_USER,
         YANDEX_MAIL_PASSWORD: process.env.YANDEX_MAIL_PASSWORD,
         YANDEX_MAIL_FROM: process.env.YANDEX_MAIL_FROM,
